9037169d215035bc16ec72ffdd69a825ad02386e
Daniel Harper <daniel.c.harper@live.com>
2021-08-27 18:05:13 -0600

    Initial Commit

---
+++ build/CMakeFiles/sortingmethods.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sortingmethods.dir/main.cpp.o"
+  "sortingmethods.pdb"
+  "sortingmethods"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sortingmethods.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
